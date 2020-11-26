16b04cb87f3a0d5a0f0837ebbf1a91b9bcd1730b
Apple-Pies <74937838+Apple-Pies@users.noreply.github.com>
2020-11-26 16:30:02 -0500

    Create models.py

---
+++ todosite/firstapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Task(models.Model):
+
+
+	MyCHOICES = (
+			('Least Important', '✨ Least Important'),
+			('Somewhat Important', '⭐ Somewhat Important'),
+			('Mandatory', '🌟 Mandatory'),
+			)
+
+	name = models.CharField(max_length=200)
+	note = models.CharField(max_length=800)
+	completion = models.BooleanField(default=False)
+	priority = models.CharField(max_length=200, null=True, choices=MyCHOICES)
+	Time_created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
