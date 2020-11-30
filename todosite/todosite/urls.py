c2c24c10b0d32c2f8af9d8ad4e20c50bc1f7cd03
Apple-Pies <sopheaj04@gmail.com>
2020-11-30 18:01:31 -0500

    ;

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('firstapp.urls')),
-    path('r^admin/', admin.site.urls), # < - remove
+    # < - remove
     url(r'^admin/', admin.site.urls),
 ]
