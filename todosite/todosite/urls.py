143840085488b0294b677110cc4be254fbc48ba2
Apple-Pies <sopheaj04@gmail.com>
2020-12-01 20:17:55 -0500

    asd

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     path('', include('firstapp.urls')),
-    path('admin/', admin.site.urls),
 ]
