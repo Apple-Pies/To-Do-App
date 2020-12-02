baa2d35e2070f141918744967dbd1c077cf42a12
Apple-Pies <sopheaj04@gmail.com>
2020-12-01 21:21:22 -0500

    some message

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('', firstapp.urls),
+    path('', include('firstapp.urls')),
 ]
