4d8c696694afc3afb7d411c55285a1f2b719fb83
Apple-Pies <sopheaj04@gmail.com>
2020-12-02 20:02:33 -0500

    please help please

--- todosite/todosite/urls.py
+++ todosite/todosite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('firstapp.urls')),
+    path('firstapp/', include('firstapp.urls')),
     path('admin/', admin.site.urls),
 ]
