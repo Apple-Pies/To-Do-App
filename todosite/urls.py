675e187e0a0047a10ccdbcce59a180249540688b
Apple-Pies <sopheaj04@gmail.com>
2020-12-05 00:03:22 -0500

    asdasdasd

--- todosite/urls.py
+++ todosite/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('firstapp.urls')),
     path('', include('firstapp.urls', namespace='firstapp')),
 
 ]
