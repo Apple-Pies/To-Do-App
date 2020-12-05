f664a00070d935691740fc53a3429a2d02188845
Apple-Pies <sopheaj04@gmail.com>
2020-12-05 00:25:39 -0500

    aa

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = 'firstapp'
 urlpatterns = [
-    path('register/', views.registerPage, name="register"),
-	path('login/', views.loginPage, name="login"),  
-	path('logout/', views.logoutUser, name="logout"),
+        path('register/', views.registerPage, name="register"),
+        path('login/', views.loginPage, name="login"),  
+        path('logout/', views.logoutUser, name="logout"),
 
-    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('task/', views.task, name="task"),
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
 
