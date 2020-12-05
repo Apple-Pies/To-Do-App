d88be39f9b5366d6b3a0d270a7da7e5d54bfee33
Apple-Pies <sopheaj04@gmail.com>
2020-12-04 23:21:53 -0500

    asasdasdas

--- firstapp/views.py
+++ firstapp/views.py
@@ -10,7 +10,7 @@ from .forms import *
 
 def registerPage(request):
 	if request.user.is_authenticated:
-		return redirect('home')
+		return redirect('/')
 	else:
 		form = CreateUserForm()
 		if request.method == 'POST':
@@ -28,7 +28,7 @@ def registerPage(request):
 
 def loginPage(request):
 	if request.user.is_authenticated:
-		return redirect('home')
+		return redirect('/')
 	else:
 		if request.method == 'POST':
 			username = request.POST.get('username')
@@ -38,7 +38,7 @@ def loginPage(request):
 
 			if user is not None:
 				login(request, user)
-				return redirect('home')
+				return redirect('/')
 			else:
 				messages.info(request, 'Username OR password is incorrect')
 
