c30a7cfba89bf5c9475ddf387f3241d349fd7262
Apple-Pies <sopheaj04@gmail.com>
2020-12-04 23:53:45 -0500

    jdasjdkajdsk lemme host already asdjnasdjsnajndjasndasd

--- todosite/urls.py
+++ todosite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('firstapp.urls')),
     path('', include('firstapp.urls', namespace='firstapp')),
 
 ]
