c1c1c647aa003f3228450a70253d79941c4f0bec
Apple-Pies <sopheaj04@gmail.com>
2020-12-06 22:03:19 -0500

    d

--- firstapp/models.py
+++ firstapp/models.py
@@ -9,7 +9,6 @@ class Task(models.Model):
 			('Mandatory', '🌟 Mandatory'),
 			)
 			
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200)
 	note = models.TextField(max_length=800)
 	completion = models.BooleanField(default=False)
